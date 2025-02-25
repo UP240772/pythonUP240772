841cc6a93f74cfcba583a78c3588a6f06dbdc436
UP240772 <up240772@alumnos.upa.edu.mx>
2025-02-25 08:37:02 -0600

    Cambios

--- Ejercicios1.py
+++ Ejercicios1.py
@@ -14,13 +14,53 @@ area = 0.5 * base * altura
 print("El area del triangulo es de: ", area)
 
 #Perimetro de un triangulo
-lado1 = float(input("Ingrese la longitud del lado 1: "))
-lado2 = float(input("Ingrese la longitud del lado 2: "))
-lado3 = float(input("Ingrese la longitud del lado 3: "))
-perimetro = lado1 + lado2 + lado3
+ladoUno = float(input("Ingrese la longitud del lado 1: "))
+ladoDos = float(input("Ingrese la longitud del lado 2: "))
+ladoTres = float(input("Ingrese la longitud del lado 3: "))
+perimetro = ladoUno + ladoDos + ladoTres
 print("El perimetro del triangulo es de: ", perimetro)
 
 #Calcular la longitud y el ancho de un rectangulo mediante un comando. Calcular su area
 longitud = float(input("Ingrese la longitud del rectangulo: "))
 ancho = float(input("Ingrese el ancho del triangulo: "))
 area = longitud * ancho
+perimetroRectangulo = 2 * (longitud + ancho)
+
+#Radio de un circulo
+radio = float(input("Ingrese la radio del circulo"))
+areaDelCirculo = 3,14 * radio **2
+Circunferencia = 2 * 3,14 * radio
+
+# Definir la ecuación
+def calcular_pendiente_intersecciones():
+    # La ecuación es y = mx + b, donde m es la pendiente y b es la intersección con el eje y
+    m = 2  # Pendiente
+    b = -2  # Intersección con el eje y
+
+    # La intersección con el eje x ocurre cuando y = 0
+    # 0 = 2x - 2 => x = b / m
+    x_interseccion = -b / m
+
+    print(f"Pendiente (m): {m}")
+    print(f"Intersección con el eje y (b): {b}")
+    print(f"Intersección con el eje x: {x_interseccion}")
+
+# Llamar a la función
+calcular_pendiente_intersecciones()
+
+import math
+
+# Definir los puntos
+punto1 = (2, 2)
+punto2 = (6, 10)
+
+Calcular la pendiente (m)
+y2, y1 = punto2[1], punto1[1]
+x2, x1 = punto2[0], punto1[0]
+pendiente = (y2 - y1) / (x2 - x1)
+
+#Calcular la distancia euclidiana
+distancia = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+
+print(f"Pendiente: {pendiente}")
+print(f"Distancia euclidiana: {distancia}")
