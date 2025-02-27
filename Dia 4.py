66cf9ba2860c50384a9f01138ed188eb7e9f18d3
UP240772 <up240772@alumnos.upa.edu.mx>
2025-02-27 11:21:43 -0600

    Cambios

--- Dia 4.py
+++ Dia 4.py
@@ -14,3 +14,17 @@ TerceraPalabra = "todos"
 Frase = PrimeraPalabra + Espacio + SegundaPalabra + Espacio + TerceraPalabra
 print(Frase)
 
+#Declare una variable llamada empresa y asígnele un valor inicial "Codificación para todos".
+Empresa = Frase
+
+#Imprima la variable empresa utilizando print() .
+print (Empresa)
+
+#Imprima la longitud de la cadena de la empresa utilizando el método len() y print() .
+print(len(Empresa))
+
+#Cambie todos los caracteres a letras mayúsculas utilizando el método upper() .
+print(Empresa.upper())
+
+#Cambie todos los caracteres a letras minúsculas utilizando el método lower() .
+print(Empresa.lower())
