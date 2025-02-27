0efe2cf82765bad5a42f6ebbc4c7f0f50ab4de2a
UP240772 <up240772@alumnos.upa.edu.mx>
2025-02-27 11:09:39 -0600

    Cambios

---
+++ Dia 4.py
@@ -0,0 +1,7 @@
+#Concatenar palabras
+Primera_palabra  = "Treinta"
+Segunda_palabra = "dias"
+Tercera_palabra = "de"
+Cuarta_palabra = "Python"
+Espacio = " "
+FraseCompleta = Primera_palabra + Espacio + Segunda_palabra + Espacio + Tercera_palabra + Espacio + Cuarta_palabra
