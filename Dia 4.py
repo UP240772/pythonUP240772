58bde2a9bb2298a3ac8d19a18632b2169f871a37
UP240772 <up240772@alumnos.upa.edu.mx>
2025-02-27 11:12:42 -0600

    Cambios

--- Dia 4.py
+++ Dia 4.py
@@ -5,3 +5,12 @@ Tercera_palabra = "de"
 Cuarta_palabra = "Python"
 Espacio = " "
 FraseCompleta = Primera_palabra + Espacio + Segunda_palabra + Espacio + Tercera_palabra + Espacio + Cuarta_palabra
+print (FraseCompleta)
+
+#Concatenar una cadena 
+PrimeraPalabra = "Codificacion"
+SegundaPalabra = "para"
+TerceraPalabra = "todos"
+Frase = PrimeraPalabra + Espacio + SegundaPalabra + Espacio + TerceraPalabra
+print(Frase)
+
