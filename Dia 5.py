01c70a29487f0c33cb945c88db6c7d74c7e201cd
UP240772 <up240772@alumnos.upa.edu.mx>
2025-03-05 09:20:40 -0600

    Cambios

---
+++ Dia 5.py
@@ -0,0 +1,20 @@
+#Declarar una lista vacía
+ListaVacia = list() 
+print(len(ListaVacia)) 
+
+#Declarar una lista con más de 5 elementos
+Frutas = ["Manzana", "Naranja", "Mango", "Platano", "Limón"]
+Vegetales = ["Tomate", "Jitomate", "Papa", "Cebolla", "Zanahoria"]
+ProductosAnimales = ["Leche", "Carne", "Huevos", "Queso", "Mantequilla"]
+TecnologiasWeb = ["C++", "JavaScript", "Python", "HTML", "CSS"]
+Paises = ["Mexico", "Estados Unidos", "Japon", "China", "Venezuela"]
+print("Frutas", Frutas)
+print("Numero de frurtas: ", len(Frutas))
+print ("Vegetales", Vegetales)
+print("Numero de vegetales", len(Vegetales))
+print("Productos de origen animal: ", ProductosAnimales)
+print("Numero de productos de origen animal: ", len(ProductosAnimales))
+print("Tecnologias web: ", TecnologiasWeb)
+print("Numero de tecnologias web: ", len(TecnologiasWeb))
+print("Paises: ", Paises)
+print("Numero de paises; ", len(Paises))
