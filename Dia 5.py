46c33f4a0f103565496cf1eb161325406d7a12df
UP240772 <up240772@alumnos.upa.edu.mx>
2025-03-05 10:24:35 -0600

    Cambios

--- Dia 5.py
+++ Dia 5.py
@@ -107,3 +107,16 @@ print(FullStack)
 FullStack.insert(6, "SQL")
 print(FullStack)
 
+#Ejercicios: Nivel 2
+edades = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
+
+#Ordena la lista y encuentra la edad mínima y máxima.
+edades.sort()
+print(edades)
+
+#Agregue la edad mínima y la edad máxima nuevamente a la lista
+edades.insert(0, "19")
+print(edades)
+edades.insert(10, "26")
+
+#Encuentra la edad media (un elemento intermedio o dos elementos intermedios divididos por dos)
