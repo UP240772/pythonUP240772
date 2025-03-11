d8b073ba4371b5d49ac9fd9623f7d71c9e8b6a44
UP240772 <up240772@alumnos.upa.edu.mx>
2025-03-11 09:32:20 -0600

    Cambios

--- Dia 6.py
+++ Dia 6.py
@@ -46,3 +46,24 @@ food_stuff_lt = (Frutas + Verduras + ProductosAnimales)
 print(food_stuff_lt)
 
 #Extraiga el elemento o los elementos del medio de la tupla food_stuff_tp o de la lista food_stuff_lt.
+food_stuff_lt = ('Manzana', 'Pera', 'Melon', 'Sandia', 'Zanahoria', 'Calabaza', 'Tomate', 'Jitomate', 'Leche', 'Queso', 'Carne', 'Seso')
+PrimerElemento = food_stuff_lt [5]
+SegundoElemento = food_stuff_lt [6]
+print(PrimerElemento)
+print(SegundoElemento)
+
+#Separa los primeros tres elementos y los últimos tres elementos de la lista food_staff_lt
+Separo1 = food_stuff_lt [0:2]
+Separo2 = food_stuff_lt [9:11]
+print(Separo1)
+print(Separo2)
+
+# Eliminar la tupla food_staff_tp por completo
+del food_stuff_tp
+
+#Comprueba si un elemento existe en una tupla:
+nordic_countries = ('Denmark', 'Finland','Iceland', 'Norway', 'Sweden')
+##Comprueba si 'Estonia' es un país nórdico
+print("Estonia" in nordic_countries)
+##Comprueba si 'Islandia' es un país nórdico
+print("Islandia" in nordic_countries)
