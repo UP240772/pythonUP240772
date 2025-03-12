ab5d879659e1d03bbfd248d182f1199e20b3cc30
UP240772 <up240772@alumnos.upa.edu.mx>
2025-03-12 10:06:24 -0600

    Cambios

--- Dia 7.py
+++ Dia 7.py
@@ -27,3 +27,48 @@ print("Con Discard descartamos un elemento pro una sola vez, hasta que se vuelve
 
 #NIVEL 2
 #Join A and B
+A = {19, 22, 24, 20, 25, 26}
+B = {19, 22, 20, 25, 26, 24, 28, 27}
+
+AB = A.union(B)
+print(AB)
+
+#Find A intersection B
+A = {19, 22, 24, 20, 25, 26}
+B = {19, 22, 20, 25, 26, 24, 28, 27}
+print(A.intersection(B))
+
+#Is A subset of B
+A = {19, 22, 24, 20, 25, 26}
+B = {19, 22, 20, 25, 26, 24, 28, 27}
+print(A.issubset(B))
+
+#Are A and B disjoint sets
+A = {19, 22, 24, 20, 25, 26}
+B = {19, 22, 20, 25, 26, 24, 28, 27}
+print(A.isdisjoint(B))
+
+#Join A with B and B with A
+AB = A.union(B)
+print(AB)
+BA = B.union(A)
+print(BA)
+
+#What is the symmetric difference between A and B
+print(B.symmetric_difference(A))
+
+#Delete the sets completely
+del A
+del B
+del AB
+del BA
+
+#NIVEL 3
+
+#Convert the ages to a set and compare the length of the list and the set, which one is bigger?
+age = [22, 19, 24, 25, 26, 24, 25, 24]
+st = set(age)
+print(st)
+
+
+
