e281921b3b33c8976c6b0f18ea79682ff51f21ff
UP240772 <up240772@alumnos.upa.edu.mx>
2025-03-18 09:36:15 -0600

    Cambios

---
+++ 09_day/02_Ejercicios.py
@@ -0,0 +1,7 @@
+#Escriba un código que califique a los estudiantes según sus puntuaciones:
+#80-100, A
+#70-79, B
+#60-69, C
+#50-59, D
+#0-49, F
+Calificacion = float(input("Ingresa tu calificacion"))
