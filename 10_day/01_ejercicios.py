7522761ec6f4efbd6f31dabbb2cb96a0365acd65
UP240772 <up240772@alumnos.upa.edu.mx>
2025-03-19 15:20:09 -0600

    Cambios

---
+++ 10_day/01_ejercicios.py
@@ -0,0 +1,19 @@
+#Itere del 0 al 10 usando el bucle for, haga lo mismo usando el bucle while.
+numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for number in numeros:
+    print(number)
+
+contar = 0
+while contar < 11:
+    print(contar)
+    contar = contar +1
+
+#Itere de 10 a 0 usando el bucle for, haga lo mismo usando el bucle while.
+Numero = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+for number in Numero:
+    print(number)
+
+contar = 0
+while contar > 11:
+    print(contar)
+    contar = contar - 1
