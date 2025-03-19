00c7d62bd78d6b5247f487142a2833564bcabfa9
UP240772 <up240772@alumnos.upa.edu.mx>
2025-03-19 14:47:11 -0600

    Cambios

--- 09_day/03_Ejercicios.py
+++ 09_day/03_Ejercicios.py
@@ -12,8 +12,28 @@ person={
     }
     }
 # * Check if the person dictionary has skills key, if so print out the middle skill in the skills list.
-if ["skills"] in person:
-      print(3, )
+if "skills" in person:
+    HabilidadMedia = len(person["skills"])//2
+    print(person['skills'][HabilidadMedia])
+else:
+    print("Error")
 #* Check if the person dictionary has skills key, if so check if the person has 'Python' skill and print out the result.
+if "skills" in person:
+    TienePython = "Python" in person['skills']
+    print(TienePython)
+else:
+    print("Error")
 #* If a person skills has only JavaScript and React, print('He is a front end developer'), if the person skills has Node, Python, MongoDB, print('He is a backend developer'), if the person skills has React, Node and MongoDB, Print('He is a fullstack developer'), else print('unknown title') - for more accurate results more conditions can be nested!
+if "skills" in person:
+    Habilidades = person['skills']
+    if set(Habilidades) == {{'JavaScript', 'React'}}:
+        print("El es desarrollador Front-end")
+    elif set(Habilidades) >= {'Node', 'Python', 'MongoDB'}:
+        print("El es desarrollador back-end")
+    elif set(Habilidades) >= {'React', 'Node', 'MongoDB'}:
+        print("Es el desarrolador Full-Stack")
+    else:
+        print("Titulo desconocido")
 #* If the person is married and if he lives in Finland, print the information in the following format: 
+if person.get('is_married') and person.get('country') == 'Finland':
+    print(f"{person['first_name']} {person['last_name']} lives in {person['country']}. He is married.")
