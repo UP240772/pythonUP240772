4c9e724bbf6b65e4c7489a84d3d64861e54ce02b
UP240772 <up240772@alumnos.upa.edu.mx>
2025-03-19 10:01:37 -0600

    Cambios

--- 09_day/02_Ejercicios.py
+++ 09_day/02_Ejercicios.py
@@ -5,5 +5,20 @@
 #50-59, D
 #0-49, F
 Calificacion = int(input("Ingresa tu calificacion"))
-if Calificacion < 49:
-     print("Tu nota es F")
+if Calificacion >= 80 and Calificacion <= 100:
+    print("Tu nota es A")
+elif Calificacion >= 70 and Calificacion <= 79:
+    print("Tu nota es B")
+elif Calificacion >= 60 and Calificacion <= 69:
+    print("Tu nota es C")
+elif Calificacion >= 50 and Calificacion <= 59:
+    print("Tu nota es D")
+else:
+    print("Tu nota es F")
+
+#Comprueba si la temporada es otoño, invierno primavera o verano
+#septiembre-octubre-noviembre, otoño
+#Diciembre-enero-febrero, invierno 
+#Marzo-abril-mayo, primavera
+#Junio-julio-agosto, Verano
+Mes = int(input("Ingrese el mes: "))
