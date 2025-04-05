12fbcd67ed05e3d140ba3e066dda07c243145e68
UP240772 <up240772@alumnos.upa.edu.mx>
2025-04-04 22:22:01 -0600

    Cambios

---
+++ 15_day/01_ejercicios.py
@@ -0,0 +1,27 @@
+print('Errores en la programacion ')
+print('Todos los codigos tienen errores para evitar replicarlos')
+
+#Syntax error
+#Index Error
+num = [1,2,3,4,5,6,7,8]
+print(num[9])
+
+##ModuleNotFoundError
+import DiamantesPalFree
+
+#AttributeError
+import range
+range.ends
+
+##KeyError
+datos = {'nombre':'Javier','up':'240029'}
+print(datos[nom])
+
+#TypeError
+print("Hola" + 3)
+
+#ValueError
+print int("Tralalero Tralala")
+
+#ImportError
+import math 2
